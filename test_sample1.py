1c02a261073f24927ba12b572c5ec917426a3e8a
edwardmfho <mf.ho92@gmail.com>
2021-06-20 10:02:33 +1000

    added unit tests for calculation

--- test_sample1.py
+++ test_sample1.py
@@ -1,10 +1,22 @@
 import pytest
-def test_file1_method1():
-	x=5
-	y=6
-	assert x+1 == y,"test failed"
-	assert x == y,"test failed"
-def test_file1_method2():
-	x=5
-	y=6
-	assert x+1 == y,"test failed" 
+import calculator as cal
+
+# Determine if calculation could be done correctly
+
+def test_sum_method1():
+	a=6
+	b=5
+	assert cal.sum(a, b) == 11, "Summation Error: Please check cal.sum()."
+
+
+def test_discrepancy_test():
+	# benchmark= 178.75
+	benchmark = 5
+	threshold = 0.05
+	a = 0.53515
+	b = 334
+
+	calculated = cal.multiply(a, b)
+	difference = abs(benchmark - calculated)
+
+	assert difference <= threshold, "Discrepancy over the threshold, further investigation required"
